cebcde2cc9292db722173e5826b9f6c68067b61f
Richard Alex Hofer <me@rofer.me>
2016-09-10 08:32:02 -0400

    Run elm-format over code.

--- src/AStar.elm
+++ src/AStar.elm
@@ -4,6 +4,7 @@ import Array exposing (Array(..))
 import Set exposing (Set(..))
 import PairingHeap as PH
 
+
 -- Returns a shortest path from a source node to a goal node.
 -- If a path does not exist an empty list is returned.
 --aStar : (comparable -> List (comparable, number))
@@ -11,36 +12,44 @@ import PairingHeap as PH
 --    -> List comparable
 --    -> Set comparable
 --    -> List comparable
+
+
 aStar getNeighbors heuristic sources goals =
-  Array.toList (aStarArray getNeighbors heuristic sources goals)
+    Array.toList (aStarArray getNeighbors heuristic sources goals)
+
 
 aStarArray getNeighbors heuristic sources goals =
     let
         --aStar' : PH.PairingHeap number (number, comparable, List comparable)
         --    -> Set comparable
         --    -> Array comparable
-        aStar' open closed = 
+        aStar' open closed =
             case (PH.findMin open) of
                 Nothing ->
                     Array.empty
 
-                Just (cost, (dist, nextNode, partPath)) ->
-                    if Set.member nextNode goals
-                       then
-                          partPath
-                       else
-                          let
-                              closed' : Set comparable
-                              closed' = Set.insert nextNode closed
+                Just ( cost, ( dist, nextNode, partPath ) ) ->
+                    if Set.member nextNode goals then
+                        partPath
+                    else
+                        let
+                            closed' : Set comparable
+                            closed' =
+                                Set.insert nextNode closed
 
-                              neighbors : List comparable
-                              neighbors = List.filter (\x -> not (Set.member x closed')) (getNeighbors nextNode)
+                            neighbors : List comparable
+                            neighbors =
+                                List.filter (\x -> not (Set.member x closed')) (getNeighbors nextNode)
 
-                              open' : PH.PairingHeap number (number, comparable, Array comparable)
-                              open' = List.foldl (\(x, d) -> 
-                                  PH.insert (dist + d + heuristic x, (dist + d, x, Array.push x partPath)))
-                                      (PH.deleteMin open) neighbors
-                          in
-                              aStar' open' closed'
+                            open' : PH.PairingHeap number ( number, comparable, Array comparable )
+                            open' =
+                                List.foldl
+                                    (\( x, d ) ->
+                                        PH.insert ( dist + d + heuristic x, ( dist + d, x, Array.push x partPath ) )
+                                    )
+                                    (PH.deleteMin open)
+                                    neighbors
+                        in
+                            aStar' open' closed'
     in
-       aStar' (PH.fromList (List.map (\x -> (heuristic x, (0, x, Array.repeat 1 x))) sources)) Set.empty
+        aStar' (PH.fromList (List.map (\x -> ( heuristic x, ( 0, x, Array.repeat 1 x ) )) sources)) Set.empty
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,54 +1,65 @@
 module Tests exposing (..)
 
 import AStar exposing (..)
-
 import Test exposing (..)
 import Dict
 import Expect
 import Set
 import String
 
+
 genGrid : List comparable -> Dict.Dict comparable ()
-genGrid coords = 
-    Dict.fromList (List.map (\x -> (x, ())) coords)
+genGrid coords =
+    Dict.fromList (List.map (\x -> ( x, () )) coords)
 
-grid1 : Dict.Dict (number, number) ()
-grid1 = 
-    genGrid [(1,1), (2,1)]
 
-grid2 : Dict.Dict (number, number) ()
-grid2 = 
-    genGrid [(1,1), (1,2), (2,1), (2,2)]
+grid1 : Dict.Dict ( number, number ) ()
+grid1 =
+    genGrid [ ( 1, 1 ), ( 2, 1 ) ]
 
-grid3 : Dict.Dict (number, number) ()
-grid3 = 
-    genGrid [(1,1), (1,2), (1,3), (2,1), (2,3), (3,1), (3,2), (3,3)]
 
-getNeighbors : Dict.Dict (number, number) a -> (number, number) -> List ((number, number), number)
-getNeighbors dict (x,y) =
+grid2 : Dict.Dict ( number, number ) ()
+grid2 =
+    genGrid [ ( 1, 1 ), ( 1, 2 ), ( 2, 1 ), ( 2, 2 ) ]
+
+
+grid3 : Dict.Dict ( number, number ) ()
+grid3 =
+    genGrid [ ( 1, 1 ), ( 1, 2 ), ( 1, 3 ), ( 2, 1 ), ( 2, 3 ), ( 3, 1 ), ( 3, 2 ), ( 3, 3 ) ]
+
+
+getNeighbors : Dict.Dict ( number, number ) a -> ( number, number ) -> List ( ( number, number ), number )
+getNeighbors dict ( x, y ) =
     let
-        possibilities = [(x, y-1), (x, y+1), (x-1, y), (x+1, y)]
-        neighbors = List.filter (\k -> Dict.member k dict) possibilities
+        possibilities =
+            [ ( x, y - 1 ), ( x, y + 1 ), ( x - 1, y ), ( x + 1, y ) ]
+
+        neighbors =
+            List.filter (\k -> Dict.member k dict) possibilities
     in
-       List.map (\k -> (k, 1)) neighbors
+        List.map (\k -> ( k, 1 )) neighbors
 
-manhattanDist : (number, number) -> (number, number) -> number
-manhattanDist (a,b) (x,y) = (abs (a - x)) + (abs (b - y))
 
-genTest : Dict.Dict (number, number) () -> (number, number) -> (number, number) -> List (number, number)
+manhattanDist : ( number, number ) -> ( number, number ) -> number
+manhattanDist ( a, b ) ( x, y ) =
+    (abs (a - x)) + (abs (b - y))
+
+
+genTest : Dict.Dict ( number, number ) () -> ( number, number ) -> ( number, number ) -> List ( number, number )
 genTest grid src dst =
-    aStar (getNeighbors grid) (manhattanDist dst) [src] (Set.singleton dst)
+    aStar (getNeighbors grid) (manhattanDist dst) [ src ] (Set.singleton dst)
+
 
 all : Test
 all =
     describe "AStar tests (Manhattan heuristic)"
         [ test "No path returns empty string" <|
             \() ->
-                Expect.equal (aStar (\x -> []) (\x -> 1) [(1,1)] (Set.singleton (2,2))) []
+                Expect.equal (aStar (\x -> []) (\x -> 1) [ ( 1, 1 ) ] (Set.singleton ( 2, 2 ))) []
         , test "Same source and dest" <|
             \() ->
-                Expect.equal (aStar (\x -> []) (\x -> 1) [(1,1)] (Set.singleton (1,1))) [(1,1)]
+                Expect.equal (aStar (\x -> []) (\x -> 1) [ ( 1, 1 ) ] (Set.singleton ( 1, 1 ))) [ ( 1, 1 ) ]
         , test "Two node grid" <|
             \() ->
-                Expect.equal (genTest grid1 (1,1) (2,1)) [(1,1), (2,1)]
+                Expect.equal (genTest grid1 ( 1, 1 ) ( 2, 1 )) [ ( 1, 1 ), ( 2, 1 ) ]
         ]
